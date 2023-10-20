c947c62f461fcff69c24b142d7350b5b4c18e840
Simon Loffler <simon.loffler@acmi.net.au>
2023-10-20 11:46:49 +1030

    Update home test to load html

--- tests/tests.py
+++ tests/tests.py
@@ -6,6 +6,6 @@ def test_root():
     Test the Works embeddings root returns expected content.
     """
     with application.test_client() as client:
-        response = client.get('/')
+        response = client.get('/?json=false')
         assert response.status_code == 200
         assert 'Works embeddings' in response.text
